fb5f8f2eb15806f241a8847eed3b277466a67b67
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-19 12:39:35 -0400

    Mumuki Export on 2018-04-19 12:39:35 -0400

--- 00016_Usando Funciónes/test..js
+++ 00016_Usando Funciónes/test..js
@@ -1,5 +1,14 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
+describe("mitad", () => {
+  it("mitad(\"cara\", \"pizzas\", \"empanadas\") es \"pizzas\"", () => {
+    assert.equal(decisionConMoneda("cara", "pizzas", "empanadas"), "pizzas")
+  })
+  it("decisionConMoneda(\"cara\", \"asado\", \"empanadas\") es \"asado\"", () => {
+    assert.equal(decisionConMoneda("cara", "asado", "empanadas"), "asado")
+  })
+  it("decisionConMoneda(\"ceca\", \"pizzas\", \"empanadas\") es \"empanadas\"", () => {
+    assert.equal(decisionConMoneda("ceca", "pizzas", "empanadas"), "empanadas")
+  })
+  it("decisionConMoneda(\"ceca\", \"pizzas\", \"pastas\") es \"pastas\"", () => {
+    assert.equal(decisionConMoneda("ceca", "pizzas", "pastas"), "pastas")
   })
 })
