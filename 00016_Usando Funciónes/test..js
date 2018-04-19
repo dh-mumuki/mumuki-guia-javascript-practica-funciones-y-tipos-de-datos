eb9c6a6abcf910abe036181bcdbada424849d70b
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-19 14:39:16 -0400

    Mumuki Export on 2018-04-19 14:39:16 -0400

--- 00016_Usando Funciónes/test..js
+++ 00016_Usando Funciónes/test..js
@@ -1,8 +1,11 @@
 describe("mitad", () => {
-  it("mitad(6) divide 6 a la mitad y nos da 3", () => {
+  it("deberia dividir 6 a la mitad para darnos 3", () => {
     assert.equal(mitad(6), 3)
   })
-  it("mitad(100) divide 100 a la mitad y nos da 50", () => {
+  it("deberia dividir 100 a la mitad para darnos 50", () => {
     assert.equal(mitad(100), 50)
+  })
+  it("deberia dividir 1 a la mitad para darnos 0.5", () => {
+    assert.equal(mitad(1), 0.5)
   })
 })
