afb10b3c386657038644ad928d4a5ce2d929a61d
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-19 17:03:45 -0400

    Mumuki Export on 2018-04-19 17:03:45 -0400

--- 00024_Formulas y Funciónes/hint.md
+++ 00024_Formulas y Funciónes/hint.md
@@ -3,5 +3,7 @@ Acordate que:
 ```javascript
  perimetro = π * r * 2;
  
- area = π * r²
+ area = π * r²;
+ 
+ π = 3.14;
 ```
--- 00024_Formulas y Funciónes/test..js
+++ 00024_Formulas y Funciónes/test..js
@@ -1,5 +1,11 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
+describe("multiplicar", function() {
+  it("deberia multiplicar 5 por 7 y deberia darnos 35", () => {
+    assert.equal(multiplicar(5,7), 35)
+  })
+  it("deberia multiplicar 7 por 10 y deberia darnos 70", () => {
+    assert.equal(multiplicar(7,10), 70)
+  })
+  it("deberia multiplicar 50 por 0.5 y deberia darnos 25", () => {
+    assert.equal(multiplicar(50,0.5), 25)
   })
 })
