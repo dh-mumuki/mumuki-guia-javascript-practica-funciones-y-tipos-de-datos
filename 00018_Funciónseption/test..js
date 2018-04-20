1dbf6e39d50f4ad03a7a17489ba213d8c4df3279
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-20 08:21:11 -0400

    Mumuki Export on 2018-04-20 08:21:11 -0400

--- 00018_Funciónseption/test..js
+++ 00018_Funciónseption/test..js
@@ -2,10 +2,10 @@ describe("esMagico", function() {
   it("deberia tomar 5 y devolvernos 12", () => {
     assert.equal(esMagico(5), 12)
   })
-  it("deberia multiplicar 7 por 10 y deberia darnos 70", () => {
-    assert.equal(multiplicar(7,10), 70)
+  it("deberia tomar 0 y devolvernos 2", () => {
+    assert.equal(esMagico(0), 2)
   })
-  it("deberia multiplicar 50 por 0.5 y deberia darnos 25", () => {
-    assert.equal(multiplicar(50,0.5), 25)
+  it("deberia tomar -4 y devolvernos -6", () => {
+    assert.equal(esMagico(-4), -6)
   })
 })
