69b4d7ae69ae13110ea4e6e2175965a97c962627
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-20 13:28:57 -0400

    Mumuki Export on 2018-04-20 13:28:57 -0400

--- 00022_Operando Strings/test..js
+++ 00022_Operando Strings/test..js
@@ -1,5 +1,11 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
+describe("longitudNombreCompleto", () => {
+  it("Con el nombre Juan Pérez deberia tener una longitud de 10", () => {
+    assert.equal(longitudNombreCompleto("Juan", "Pérez"), 10)
+  })
+  it("Con el nombre María de las Nieves deberia tener una longitud de 19", () => {
+    assert.equal(longitudNombreCompleto("María", "de las Nieves"), 19)
+  })
+  it("Con el nombre Jet Li deberia tener una longitud de 6", () => {
+    assert.equal(longitudNombreCompleto("Jet", "Li"), 6)
   })
 })
