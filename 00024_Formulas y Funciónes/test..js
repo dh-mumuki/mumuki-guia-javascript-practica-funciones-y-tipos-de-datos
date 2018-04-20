f2677c1e60a54073a0de29f5f8ac78f0e502725e
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-20 08:42:14 -0400

    Mumuki Export on 2018-04-20 08:42:14 -0400

--- 00024_Formulas y Funciónes/test..js
+++ 00024_Formulas y Funciónes/test..js
@@ -1,11 +1,32 @@
-describe("multiplicar", function() {
-  it("deberia multiplicar 5 por 7 y deberia darnos 35", () => {
-    assert.equal(multiplicar(5,7), 35)
+describe("perimetro", function() {
+  it(" de radio 4 nos deberia devolver 25.12", () => {
+    assert.equal(perimetro(4), 25.12)
   })
-  it("deberia multiplicar 7 por 10 y deberia darnos 70", () => {
-    assert.equal(multiplicar(7,10), 70)
+})
+describe("area", function() {
+  it("de radio 4 deberia devolver 50.24", () => {
+    assert.equal(area(4), 50.24)
   })
-  it("deberia multiplicar 50 por 0.5 y deberia darnos 25", () => {
-    assert.equal(multiplicar(50,0.5), 25)
+})
+
+describe("perimetro", function() {
+  it(" de radio 1 nos deberia devolver 6.28", () => {
+    assert.equal(perimetro(1), 6.28)
+  })
+})
+describe("area", function() {
+  it("de radio 1 deberia devolver 3.14", () => {
+    assert.equal(area(1), 3.14)
+  })
+})
+
+describe("perimetro", function() {
+  it(" de radio 50 nos deberia devolver 314", () => {
+    assert.equal(perimetro(50), 314)
+  })
+})
+describe("area", function() {
+  it("de radio 50 deberia devolver 7850", () => {
+    assert.equal(area(50), 7850)
   })
 })
